3bf3d19767e1a617f7eee057c79239404cd029a7
rijwankabir <rijwan241-15-840@diu.edu.bd>
2025-06-06 17:24:25 +0600

    Add Display

--- sec_proj.js
+++ sec_proj.js
@@ -2,15 +2,16 @@ function multiply(a, b) {
     return a * b;
 }
 
+let result = document.getElementById("display");
 let Num = Number(prompt("Enter a number:"));
 
 if (Num % 2 === 0)
-    console.log(`${Num} is even.`);
-else 
-    console.log(`${Num} is odd.`);
+    result.innerHTML = `${Num} is even.`;
+else
+    result.innerHTML = `${Num} is odd.`;
 
 let num1 = Number(prompt("Enter the first number to multiply:"));
 let num2 = Number(prompt("Enter the second number to multiply:"));
 
 let product = multiply(num1, num2);
-console.log(`The product of ${num1} and ${num2} is: ${product}`);
+result.innerHTML += `<br>The product of ${num1} and ${num2} is: ${product}`;
